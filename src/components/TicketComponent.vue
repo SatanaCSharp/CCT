f04e76871f25ca6002055598f26f801a4f55129b
Yuri.G <Yuri.G>
2018-08-10 15:20:53 +0300

    It was created tickets fo displaing data of crypto carency

---
+++ src/components/TicketComponent.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="ticket-currency">
+        <span class="name-of-currency">BTC</span>
+        <span class="price-of-currency">6700.83$</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TicketComponent"
+    }
+</script>
+
+<style scoped>
+    .ticket-currency{
+        margin-top: 20px;
+        margin-right: 20px;
+        flex-basis: 23%;
+        width: 200px;
+        height: 100px;
+        background-color: #4b4b4b;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        border-radius: 5px;
+        color: #cccccc;
+        position: relative;
+    }
+
+    .name-of-currency{
+        display: inline-flex;
+        font-size: 20px;
+        line-height: 25px;
+    }
+
+    .name-of-currency:before {
+        content: '';
+        width: 25px;
+        height: 25px;
+        background: url(../assets/btc.png) no-repeat center;
+        background-size: contain;
+        margin-right: 7px;
+        display: inline-block;
+    }
+    .price-of-currency:before {
+        content: '';
+        display: block;
+        height: 1px;
+        margin: 10px 0;
+        background-color: #ccc;
+    }
+    
+    
+</style>
